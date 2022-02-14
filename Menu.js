72732d4ccd5dd6c1f220498f795fa7dc6faeaf2b
spoiledragon <karlo.gallegos2031@alumnos.udg.mx>
2022-02-14 10:01:26 -0600

    Se ha creado el navigation en la Ventana 2 y se dio la configuracion en Menujs

---
+++ Menu.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import lOGIN from './Ventana2';
+import REGISTRO from './Registro';
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const Stack = createNativeStackNavigator();
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false,}}>
+          <Stack.Screen name="Login" component={lOGIN} />
+          <Stack.Screen name="Register" component={REGISTRO} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
